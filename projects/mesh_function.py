6c204f9f47bfc460c9ecf50a303040979c9d7d75
Juan-Pablo-Villa-J <jpvilla95@outlook.com>
2025-09-03 09:38:14 +0000

    Fixing yet another error

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -17,7 +17,7 @@ def func(t: float) -> float:
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
-    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
+    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-4)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
 
