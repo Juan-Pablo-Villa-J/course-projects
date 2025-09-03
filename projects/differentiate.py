cede5eb8f69ca6c39ff6037985af903c91f8bbb1
Juan-Pablo-Villa-J <jpvilla95@outlook.com>
2025-09-03 08:34:41 +0000

    Fixing differentiate

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,19 +2,19 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
-    #d[0]=(u[0]+u[1])/2
     d[0]=(u[0]+u[1])/2
     for j in range(1,len(u))
         d[j]=(u[j]+u[j+1])/2
     return d
+    #raise NotImplementedError
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
     u_lag=u[:-1]
     u_forward=u[1:]
     d[0]=(u[0]+u[1])/2
     d[1:]=(u_lag+u_forward)/2
-
     return d
+    #raise NotImplementedError
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
