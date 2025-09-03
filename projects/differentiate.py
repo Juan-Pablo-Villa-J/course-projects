e3c99abdececf9fbccd6c5b037c024c7fbe132c3
Juan-Pablo-Villa-J <jpvilla95@outlook.com>
2025-09-03 15:27:57 +0200

    Update differentiate.py once more

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -5,7 +5,7 @@ def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
     N=len(u)                        # Good idea to keep this as a parameter
     d = np.zeros(N)                 # Initialize the output array
     d[0]=(u[1]-u[0])/dt                # Set the first element with forward difference
-    d[-1]=(u[-1]+u(-2))/dt            # Set last element with backward difference
+    d[-1]=(u[-1]-u[-2])/dt            # Set last element with backward difference
     for j in range(1,N-1,1):        # Set counter in the middle
         d[j]=(u[j+1]-u[j-1])/dt/2        # Use mid-points
     return d
@@ -14,7 +14,7 @@ def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
     N=len(u)                               # It is nice to have an N
     d = np.zeros(N)                      # Initialize the output array
     d[0]=(u[1]-u[0])/dt                # Set the first element with forward difference
-    d[-1]=(u[-1]+u(-2))/dt            # Set last element with backward difference
+    d[-1]=(u[-1]-u[-2])/dt            # Set last element with backward difference
     d[1:-2]=(u[1:]-u[:-1])/dt/2        #    Compute with mid-points
     return d
 
