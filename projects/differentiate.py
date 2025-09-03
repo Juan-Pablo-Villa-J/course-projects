Fixing sign errors

projects/differentiate.py
@@ -12,8 +12,8 @@ def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
     d = np.zeros_like(u)  # Initialize the output array
     u_lag=u[:-1]
     u_forward=u[1:]
-    d[0]=(u[0]-u[1])/dt
-    d[1:]=(u_lag-u_forward)/dt
+    d[0]=(u[1]-u[0])/dt
+    d[1:]=(u_forward-u_lag)/dt
     return d
 
 